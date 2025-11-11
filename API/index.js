e518c646fe296946dc90a8f85f227ea6c54c7d41
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-11-11 13:06:23 +0530

    commit-30

--- API/index.js
+++ API/index.js
@@ -1,11 +1,11 @@
-let price = document.getElementById("btc-price");
+let priceText = document.getElementById("price-value");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function updatePrice() {
   fetch("https://api.coinbase.com/v2/prices/BTC-USD/spot")
     .then((response) => response.json())
     .then((data) => {
-      price.textContent = `Current Bitcoin Price: $${data.data.amount}`;
+      priceText.textContent = `$${(Math.round(data.data.amount * 100) / 100)}`;
     })
     .catch((error) => {
       console.error("Error fetching Bitcoin price:", error);
@@ -13,5 +13,7 @@ btn.addEventListener("click", function updatePrice() {
     .finally(() => {
       console.log("Price update attempt finished.");
     });
-setTimeout(updatePrice, 1000);
+
+  setTimeout(updatePrice, 1000);
+  setInterval(updatePrice, 5000);
 });
