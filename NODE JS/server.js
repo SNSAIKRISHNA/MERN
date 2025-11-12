da3df98adfaee8ad145cf155fe20a9a29f63e541
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-11-12 11:06:25 +0530

    commit-31 (GET data with REST API)

--- NODE JS/server.js
+++ NODE JS/server.js
@@ -1,12 +1,29 @@
-import http from 'node:http';
+import http from "node:http";
+import { getDataFromDB } from "./Database/db.js";
 
-const server = http.createServer((req, res) => {
-    res.end("Server is running...")
+const PORT = 8000;
 
-})
+const server = http.createServer(async (req, res) => {
+  const destination = await getDataFromDB();
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.end(JSON.stringify(destination));
+  } else if (req.url.startsWith("/continent") && req.method === "GET") {
+    const continent = req.url.split("/").pop();
+    const filteredDestinations = destination.filter((destination) => {
+      return destination.continent.toLowerCase() === continent.toLowerCase();
+    });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(filteredDestinations));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
 
-server.listen(8000 ,() => {
-
-    console.log("Server is running on port 8000...");
-
-})
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
