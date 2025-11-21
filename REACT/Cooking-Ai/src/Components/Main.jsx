d3888cb2a7fedb56e5e541821541cecb740e2907
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-11-21 19:17:35 +0530

    commit-46

--- REACT/Cooking-Ai/src/Components/Main.jsx
+++ REACT/Cooking-Ai/src/Components/Main.jsx
@@ -1,16 +1,19 @@
-export default function Main() {
-  const ingredient = ["Chicken", "Salt", "Pepper"];
+import React from "react";
 
-  const ingredientList = ingredient.map((item) => {
-    return <li key={item}>{item}</li>;
+export default function Main() {
+
+ let [ingredient, setIngredient] = React.useState([]); // State to hold the list of ingredients //initially an empty array
+
+  const ingredientList = ingredient.map((item) => {    // Mapping through the ingredients array to create list items //item is each ingredient in the array //map returns a new array
+    return <li key={item}>{item}</li>;                 // Using item as key assuming ingredients are unique 
   });
 
   function handelSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const newIngredient = formData.get("ingredient");
-    ingredient.push(newIngredient);
-    console.log(ingredient);
+    const formData = new FormData(event.currentTarget); // Creating a FormData object from the form element //event.currentTarget refers to the form element that triggered the submit event
+    const newIngredient = formData.get("ingredient"); // Getting the value of the input field named "ingredient" //formData is used to extract data from the form
+    setIngredient((prevIngredients) => [...prevIngredients, newIngredient]); // Update state with new ingredient //prevIngredients is used to get the previous state of ingredients
+    event.currentTarget.reset();
   }
 
   return (
