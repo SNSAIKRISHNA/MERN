dbef55df38866b56fbe026bd61a0b8b2a2c28b7b
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-11-22 13:04:14 +0530

    commit-48

--- REACT/Cooking-Ai/src/Components/Navbar.jsx
+++ REACT/Cooking-Ai/src/Components/Navbar.jsx
@@ -1,11 +1,10 @@
-import chefs from '../assets/chef-claude-icon.png'
-
+import chefs from "../assets/chef-claude-icon.png";
 
 export default function Header() {
-    return (
-        <header>
-            <img src={chefs} alt="Chef Claude"/>
-            <h1>FoodieFriend AI</h1>
-        </header>
-    )
+  return (
+    <header>
+      <img src={chefs} alt="Chef Claude" />
+      <h1>FoodieFriend AI</h1>
+    </header>
+  );
 }
