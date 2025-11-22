5d2e1930aa9e5cc81621c9e81a0e98e30997ef38
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-11-22 18:50:44 +0530

    commit-50

---
+++ REACT/Cooking-Ai/src/Components/ingredient.jsx
@@ -0,0 +1,23 @@
+export default function Ingredients({ ingredients, ingredientsListItems, toggleShowRecipe }) {
+  return (
+    <section>
+
+      <h2>Ingredients on hand:</h2>
+      
+      <ul className="ingredients-list" aria-live="polite">
+        {ingredientsListItems}
+      </ul>
+
+      {ingredients.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={toggleShowRecipe}>Get a recipe</button>
+        </div>
+      )}
+
+    </section>
+  );
+}
